1eca8bb0907413477d6b753833b7674b5bd3e2c0
Oleksandr <Sashapylypenko.pov@gmail.com>
2025-06-07 17:01:59 +0300

    feat: change to css

--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./containers/App";
 
-import "./styles/global.scss";
+import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
